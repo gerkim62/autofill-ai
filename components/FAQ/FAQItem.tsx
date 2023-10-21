5483a023940834bb862b952447a18ed8ad3fcadb
Gerison Kimathi Muriungi <72649107+gerkim62@users.noreply.github.com>
2023-10-21 18:11:55 +0000

    fixed faq

--- components/FAQ/FAQItem.tsx
+++ components/FAQ/FAQItem.tsx
@@ -9,7 +9,7 @@ interface Props {
 export default function Faq({ question, answer }: Props): ReactElement {
 
   return (
-    <div className="my-2 mx-4 p-4 rounded-lg -ml-5">
+    <div className="my-2 mx-4 p-4 rounded-lg -ml-12">
       <div className="flex items-center text-pink-500">
         <FaQuestion className="mr-2" /> 
         <h3 className="text-lg font-medium">{question}</h3>
