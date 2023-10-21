658b668bfbbe46d7eb59245735888d0790946edb
Gerison Kimathi Muriungi <72649107+gerkim62@users.noreply.github.com>
2023-10-21 19:10:13 +0000

    added cute msg

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,19 @@
-import Link from 'next/link';
-import React from 'react';
+import Link from "next/link";
+import React from "react";
 
 const LandingPage = () => {
   return (
     <div className="bg-white flex items-center justify-center mt-20">
       <div className="text-center">
-        <h1 className="sm:text-4xl text-2xl font-bold text-pink-500 mb-4">Welcome to Autofill AI</h1>
-        <p className="text-gray-600 text-lg mb-8">Your Ultimate Study Companion</p>
+        <h1 className="sm:text-4xl text-2xl font-bold text-pink-500 mb-4">
+          Welcome to Autofill AI
+        </h1>
+        <p className="text-gray-600 text-lg mb-8">
+          Your Ultimate Study Companion
+        </p>
         <p className="text-gray-800 text-xl mb-8 mx-2">
-          Autofill AI is a powerful tool designed to help you excel in your studies.
+          Boost Your Grades with Autofill AI - Your Secret to Acing Online
+          Exams!
         </p>
         <div className="mb-12">
           <Link
@@ -24,16 +29,18 @@ const LandingPage = () => {
             FAQ
           </Link>
         </div>
-  
+
         <p className="text-gray-600 text-sm">
-          By using Autofill AI, you agree to our   <a
+          By using Autofill AI, you agree to our{" "}
+          <a
             href="https://autofillai.com/terms"
             target="_blank"
             rel="noopener noreferrer"
             className="text-pink-600 hover:underline whitespace-nowrap"
           >
             Terms and Conditions
-          </a>.
+          </a>
+          .
         </p>
       </div>
     </div>
